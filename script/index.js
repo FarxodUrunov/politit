178722ef8b4cf3ab41beeb53411678aef8f400b7
Farxod <urunovfar@gmail.com>
2023-05-17 13:57:38 +0500

    end

--- script/index.js
+++ script/index.js
@@ -1,26 +1,9 @@
-const carousel = document.querySelector('.carousel');
-const item = carousel.querySelectorAll('.ui-card')[0];
+const searchInput = document.querySelector('#search__input'),
+    searchBtn = document.querySelector('#search__btn')
 
-const buttons = document.querySelectorAll('.team__section__btn button');
-
-
-let firstItemWidth = item.clientWidth + 32;
-let scrollWidth = carousel.scrollWidth - carousel.clientWidth
-
-const showHideButton = () => {
-    carousel.scrollLeft == 0 ? buttons[0].classList.add('btn__disabled') : buttons[0].classList.remove('btn__disabled');
-    carousel.scrollLeft == scrollWidth ? buttons[1].classList.add('btn__disabled') : buttons[1].classList.remove('btn__disabled');
-
-}
-
-buttons.forEach(btn => {
-    btn.addEventListener("click", () => {
-        carousel.scrollLeft += btn.id == 'left' ? -firstItemWidth : firstItemWidth
-        setTimeout(() => showHideButton(), 60)
-    })
+searchBtn.addEventListener('click', () => {
+    searchInput.classList.toggle('d-lg-none')
 })
-
-
 
 
 
